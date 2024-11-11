dfa64c90937b8b55a3e5dce95a33663dd1873e09
Miguel Urena <urena_miguel82@outlook.com>
2024-11-11 13:43:35 -0800

    Updated connection

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ app.get('/', (req, res) => {
   res.send('Hello from Node API Server Updated');
 });
 
-mongoose.connect(
-  "mongodb+srv://urenamiguel82:WKVoi7GU1phWPU4o@backenddb.avkzj.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB",
-)
-.then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(3000, () => {
-    console.log("Listening on port 3000");
+mongoose
+  .connect(
+    "mongodb+srv://urenamiguel82:WKVoi7GU1phWPU4o@backenddb.avkzj.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Error connecting to MongoDB");
   });
-})
-.catch(() => {
-  console.log("Error connecting to MongoDB");
-});
